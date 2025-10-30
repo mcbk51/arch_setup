1c3da85bb842ddddbdf2ead19616db8c7e169bb6
Marco Castellanos <marcopoulut@gmail.com>
2025-10-30 09:51:22 -0700

    changig min drive size to 30

--- full_setup_test.sh
+++ full_setup_test.sh
@@ -66,7 +66,8 @@ DRIVE_SIZE=$(lsblk -b -d -n -o SIZE "$DRIVE")
 MIN_SIZE=$((30 * 1024 * 1024 * 1024))  # 30GB in bytes
 #MIN_SIZE=$((125 * 1024 * 1024 * 1024))  # 125GB in bytes
 if [ "$DRIVE_SIZE" -lt "$MIN_SIZE" ]; then
-    print_error "Drive too small! Need at least 125GB"
+    print_error "Drive too small! Need at least 30GB"
+    #print_error "Drive too small! Need at least 125GB"
     exit 1
 fi
 
