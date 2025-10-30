b8816e2d3984ef109f2c54ce1f36d72a4f30ddcb
Marco Castellanos <marcopoulut@gmail.com>
2025-10-30 09:23:56 -0700

    adding size check

--- full_setup.sh
+++ full_setup.sh
@@ -61,6 +61,14 @@ fi
 # Partition the drive
 print_info "Partitioning $DRIVE..."
 
+#Checking Drive Size
+DRIVE_SIZE=$(lsblk -b -d -n -o SIZE "$DRIVE")
+MIN_SIZE=$((125 * 1024 * 1024 * 1024))  # 125GB in bytes
+if [ "$DRIVE_SIZE" -lt "$MIN_SIZE" ]; then
+    print_error "Drive too small! Need at least 125GB"
+    exit 1
+fi
+
 # Determine partition naming scheme
 if [[ $DRIVE == *"nvme"* ]] || [[ $DRIVE == *"mmcblk"* ]]; then
     PART1="${DRIVE}p1"
